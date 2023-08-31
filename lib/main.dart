90ba76aa6cca2910c138e3384035d973257591fe
kri-sh27 <hogalekrishna2000@gmail.com>
2023-08-31 22:53:36 +0530

    done some changes

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,98 @@ class Home extends StatelessWidget {
                 CircleAvatar(
                   radius: 60,
                 ),
+                SizedBox(
+                  width: 30,
+                ),
                 Column(
-                  children: <Widget>[Text("Name"), Text("Designation")],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Krishnat Hogale",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Designation",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
                 )
               ],
-            )
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.school,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("University of Mumbai",
+                          style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.computer_rounded, size: 40),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("computer engineer", style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_pin,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("Pune", style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("krishnahoagel@gmail.com",
+                          style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "0123456789",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Text("Abot me"),
+            Text("created by Krishna")
           ],
         ),
       ),
